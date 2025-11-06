a58b9081d38bfd20f5844bccc18226c078d6dbb7
FinianHorrie <finian.horrie@gmail.com>
2025-11-06 15:25:28 +0100

    Improved camera responsiveness on mobile

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this) 
+        { 
+            Destroy(this); 
+        } 
+        else 
+        { 
+            Instance = this; 
+        } 
+    }
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        QualitySettings.vSyncCount = 0;
+    }
+}
