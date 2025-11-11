c874312b50b758dbc9c5254358fc26621824101d
FinianHorrie <finian.horrie@gmail.com>
2025-11-11 16:20:16 +0100

    Updated scene

--- Assets/Scripts/Constants/Constants.cs
+++ Assets/Scripts/Constants/Constants.cs
@@ -2,13 +2,6 @@ using UnityEngine;
 
 static public class Constants
 {
-    #region CarVisuals
-
-    //public static readonly Color RearLightsActiveColor = new(1f, 0.3f, 0.3f, 1f);
-    //public static readonly Color RearLightsInActiveColor = new(0.5f, 0.2f, 0.2f, 1f);
-
-    #endregion
-
     #region MaterialPropertyIds
 
     public static readonly int ColorPropertyId = Shader.PropertyToID("_Color");
