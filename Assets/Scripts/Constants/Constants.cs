ff8995e6bbbb77a0b3d5c99b64f175264568b576
FinianHorrie <finian.horrie@gmail.com>
2026-03-07 18:58:43 +0100

    Updated layer tags to include player and checkpoint

--- Assets/Scripts/Constants/Constants.cs
+++ Assets/Scripts/Constants/Constants.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 static public class Constants
 {
+    #region LayerNames
+    
+    public const string PlayerLayerName = "Player";
+    
+    #endregion
+    
     #region MaterialPropertyIds
 
     public static readonly int ColorPropertyId = Shader.PropertyToID("_Color");
