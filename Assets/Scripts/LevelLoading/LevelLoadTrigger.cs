34d918524599b858d8aa4b77a88b00bc85bc567f
FinianHorrie <finian.horrie@gmail.com>
2026-03-15 16:56:15 +0100

    Added simple level load trigger

---
+++ Assets/Scripts/LevelLoading/LevelLoadTrigger.cs
@@ -0,0 +1,26 @@
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#region RaceGame
+
+public class LevelLoadTrigger : MonoBehaviour
+{
+    #region Editor Fields
+
+    [SerializeField] private string _mapName = string.Empty;
+
+    #endregion
+
+    #region Methods
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.layer != LayerMask.NameToLayer(Constants.PlayerLayerName)) return;
+        SceneManager.LoadSceneAsync(_mapName, LoadSceneMode.Single);
+    }
+
+    #endregion
+}
+
+#endregion
